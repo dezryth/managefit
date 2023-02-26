d5131f6a205650425f8917c26ae7dfe81c9edd7a
Scott Christian <scott@codesilver.io>
2023-02-26 09:28:43 -0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -106,14 +106,13 @@ function processRequest(req) {
 
 function getInspiration() {
   var position = randomInteger(0, 1642); // There are 1643 inspirational quotes in our quotes file.
-  var quote = quotes[position].text.replace("'", "");
-  var inspiration = "'" + quote + "'";
+  var quote = quotes[position].text;
   // Some quotes don't have an author, so don't specify one if null
   if (quotes[position].author) {
     inspiration += " - " + quotes[position].author;
   }
 
-  return inspiration;
+  return quote;
 }
 
 function initialize() {
